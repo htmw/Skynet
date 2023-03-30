d20f9243da0c9d0dcc3d704b9d899725ac1f619b
Josh Krinsky <95884174+Jkrin393@users.noreply.github.com>
2023-03-29 23:58:34 -0400

    correct UPS code, previous commit had test environment

--- frontend/src/components/tracking/trackingUPS.js
+++ frontend/src/components/tracking/trackingUPS.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
-import './trackingstyle.css';
 
+function TrackingPage() {
+  const [trackingNumber, setTrackingNumber] = useState('');
+  const [status, setStatus] = useState('');
 
+  const handleTrackingNumberChange = (event) => {
+    setTrackingNumber(event.target.value);
+  };
 
-function TrackingUPS() {
-    const [trackingNumber, setTrackingNumber] = useState('');
-    const [status, setStatus] = useState('');
-  
-    const handleTrackingNumberChange = (event) => {
-      setTrackingNumber(event.target.value);
-    };
-  
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const response = await axios.get(`https://wwwcie.ups.com/track/api/Track/GetStatus?loc=en_US&tracknum=${trackingNumber}`);
-        setStatus(response.data.TrackResponse.Shipment.Package.Activity[0].Status.StatusType.Description);
-      } catch (error) {
-        setStatus('Error: Unable to retrieve tracking information.');
-      }
-    };
-  
-    return (
-      <div >
-        <h1 class="tracking-header">UPS Tracking</h1>
-        <form onSubmit={handleFormSubmit}>
-          <label>
-            Tracking Number:
-            <input type="text" value={trackingNumber} onChange={handleTrackingNumberChange} />
-          </label>
-          <button type="submit">Track</button>
-        </form>
-        {status && <div>Status: {status}</div>}
-      </div>
-    );
-  }
-  
-  export default TrackingUPS;
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.get(`https://onlinetools.ups.com/track/v1/details/${trackingNumber}`, {
+        headers: {
+          "AccessLicenseNumber": 'vBp1ER70HisKWtX5jr7GNyrdhhCrmrkNtQIRADaYAfAhwQhS',
+          "Content-Type": "application/json"
+        }
+      });
+      setStatus(response.data.trackDetails[0].shipmentProgressActivities[0].statusType.description);
+    } catch (error) {
+      setStatus('Error: Unable to retrieve tracking information.');
+    }
+  };
+
+  return (
+    <div class = "tracking-output">
+      <h1 class = "tracking-header">UPS Tracking</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Tracking Number:
+          <input type="text" value={trackingNumber} onChange={handleTrackingNumberChange} />
+        </label>
+        <button type="submit">Track</button>
+      </form>
+      {status && <div>Status: {status}</div>}
+    </div>
+  );
+}
+
+export default TrackingPage;
